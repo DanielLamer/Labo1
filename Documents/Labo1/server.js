5415c46dffbd32edc5e466cca73073970182563a
DanielLamer <72472021+DanielLamer@users.noreply.github.com>
2021-09-18 19:21:36 -0400

    Delete Documents/Labo1 directory

--- Documents/Labo1/server.js
+++
@@ -1,4 +0,0 @@
-const http = require('http');
-const server = require('./route.js');
-const port = process.env.PORT || 5000;
-server.listen(port,  () => console.log(`Server running on port ${port}`))
